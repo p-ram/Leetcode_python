605910642dfd976110eda83ab793eb9226605248
p-ram <94652181+p-ram@users.noreply.github.com>
2021-12-27 23:21:23 -0800

    Update Merge_Sorted_array.py

--- Merge_Sorted_array.py
+++ Merge_Sorted_array.py
@@ -1,4 +1,4 @@
-#WIP
+# code working for normal cases, edge cases WIP
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
@@ -8,25 +8,49 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        arr=[None]*(m+n)
-        i=0
-        while (i<m):
-            arr[i]=nums1[i]
-            i+=1
-        
+        # k count for new nums1 
+        # i count for nums1
+        # j count for nums2
         k=0
-        h=0
+        i=0
         j=0
+        
+        #duplicate array nums1
+        arr=[None]*(m+n)
+        for w in range (len(nums1)):
+            arr[w]=nums1[w]
+        #end duplicate array
+        
+        
         for k in range (m+n):
-            print(k)
-            print(j)
-            print(h)
-            if(arr[j]<=nums2[h]):
-                nums1[k]=arr[j]
-                j+=1
-                print("asd",nums1)
             
-            elif(arr[j]>nums2[h]):
-                nums1[k]=nums2[h]
-                h+=1
-                print(nums1)
+            if(nums1[i]<=nums2[j] and i<(m)):
+                nums1[k]=arr[i]
+                i+=1
+                print("1",nums1)
+                 
+                
+            elif(nums1[i]>nums2[j] and j<(n)):
+                nums1[k]=nums2[j]
+                j+=1
+                print("2",nums1)
+                
+            
+            elif(i==(m) and j<(n)):
+                nums1[k]=nums2[j]
+                j+=1
+                print(k)
+                print(j)
+                print("3",nums1)
+                
+            
+            elif(j==(n)):
+                nums1[k]=arr[i]
+                i+=1
+                print("4",nums1)
+                
+            print("k",k)
+            print("i",i)
+            print("j",j)
+            print("----------------------")
+            
