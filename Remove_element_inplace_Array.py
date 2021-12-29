6bb5e8c3c1c274da826ebe83bc79e9190ef07265
p-ram <94652181+p-ram@users.noreply.github.com>
2021-12-29 13:50:07 -0800

    Update Remove_element_inplace_Array.py

--- Remove_element_inplace_Array.py
+++ Remove_element_inplace_Array.py
@@ -9,7 +9,7 @@ class Solution(object):
         #j pointed to last 
         j=len(nums)-1
         
-        #k is total elements shifted to last
+        #vc is total elements shifted to last
         vc=0
         i=0
         
@@ -19,47 +19,25 @@ class Solution(object):
             if(i==j and nums[i]==val):
                 vc+=1
                 i+=1
-                print("--1--")
-                print(i)
-                print(j)
-                print("----------")
-                
-                       
+                          
             elif(nums[i]==val):
                 temp = nums[i]
-                #nums[i]=nums[i+1]
-                
                 
                 #shift remaining elements to left
                 for m in range (i, len(nums)-1):
                     nums[m]=nums[m+1]
                 
-                
                 vc+=1
                 nums[j]=temp
                 j-=1
                 
-                print("before",i)
                 #check if nums[i+1]=val and dont increment i
                 if(nums[i+1]==val):
                     continue
-                
-                print("after",i)
-                
-                print(j)
-                print("----------")
-                
-            
+                           
             elif(nums[i]!=val):
                 i+=1
-                print("--2--")
-                print(i)
-                print(j)
-                print("----------")
         
         k=len(nums)-vc
-        print("end of while")
-        print("k",k)
-        print(nums)
         
         return k
